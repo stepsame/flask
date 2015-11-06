3ca625bf743f2a596e6e028d0343c7f2de729549
stepsame <stepsame@gmail.com>
2015-11-06 11:00:46 +0800

    finish chapter 8

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-	FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
+	MAIL_SERVER = 'smtp.gmail.com'
+	MAIL_PORT = 465
+	MAIL_USE_SSL = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'stepsame@gmail.com'
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'movmxgrmksonxjno'
+	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
@@ -15,11 +20,7 @@ class Config:
 
 class DevelopmentConfig(Config):
 	DEBUG = True
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
