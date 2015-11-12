063b8b56511daf80bdc5dd1c8c80c2e80600a623
stepsame <stepsame@gmail.com>
2015-11-12 16:32:24 +0800

    finish chapter 14

---
+++ app/api_1_0/users.py
@@ -0,0 +1,55 @@
+from flask import jsonify, request, current_app, url_for
+from . import api
+from ..models import User, Post
+
+
+@api.route('/users/<int:id>')
+def get_user(id):
+	user = User.query.get_or_404(id)
+	return jsonify(user.to_json())
+
+
+@api.route('/users/<int:id>/posts/')
+def get_user_posts(id):
+	page = request.args.get('page', 1, type=int)
+	user = User.query.get_or_404(id)
+	pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+		page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+		error_out=False)
+	posts = pagination.items
+	prev = None
+	if pagination.has_prev:
+		prev = url_for('api.get_user_posts', page=pagination.prev_num, _external=True)
+	next = None
+	if pagination.has_next:
+		next = url_for('api.get_user_posts', page=pagination.next_num, _external=True)
+	return jsonify({
+		'posts': [post.to_json() for post in posts],
+		'prev': prev,
+		'next': next,
+		'count': pagination.total
+	})
+
+
+@api.route('/users/<int:id>/timeline/')
+def get_user_followed_posts(id):
+	page = request.args.get('page', 1, type=int)
+	user = User.query.get_or_404(id)
+	pagination = user.followed_posts.order_by(Post.timestamp.desc()).paginate(
+		page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+		error_out=False)
+	posts = pagination.items
+	prev = None
+	if pagination.has_prev:
+		prev = url_for('api.get_user_followed_posts', page=pagination.prev_num,
+						_external=True)
+	next = None
+	if pagination.has_next:
+		next = url_for('api.get_user_followed_posts', page=pagination.next_num,
+						_external=True)
+	return jsonify({
+		'posts': [post.to_json() for post in posts],
+		'prev': prev,
+		'next': next,
+		'count': pagination.total
+	})
